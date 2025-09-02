3eb1f4f9347a714b63be950535b6c710fa8bc34b
IrisDonVolt <irisodonvolt@gmail.com>
2025-09-03 01:01:31 +0530

    Sign up/Log in switching

--- app/src/main/java/com/idv/campfire/MainActivity.java
+++ app/src/main/java/com/idv/campfire/MainActivity.java
@@ -1,14 +1,48 @@
 package com.idv.campfire;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtUsername, edtEmail, edtPassword;
+
+    AppCompatButton btnSubmit;
+    TextView txtSignLogInfo;  // text that displays below the button
+    private boolean isSignUp = true; // variable to keep track of which page we are in
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        txtSignLogInfo = findViewById(R.id.txtSignUp);
+
+        txtSignLogInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSignUp) {
+                    isSignUp = false;
+                    btnSubmit.setText("LOG IN");
+                    txtSignLogInfo.setText("Don't have an account? Sign up");
+                    edtUsername.setVisibility(View.GONE);
+                }
+                else {
+                    isSignUp = true;
+                    btnSubmit.setText("SIGN UP");
+                    txtSignLogInfo.setText("Already have an account? Log in");
+                    edtUsername.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
