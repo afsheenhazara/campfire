56fc694807ecfbaa80d8e8fe6fc1de4f91bf64c6
IrisDonVolt <irisodonvolt@gmail.com>
2025-11-02 20:13:45 +0530

    Started with MessageActivity functionaity

--- app/src/main/java/com/idv/campfire/FriendsActivity.java
+++ app/src/main/java/com/idv/campfire/FriendsActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,6 +29,7 @@ public class FriendsActivity extends AppCompatActivity {
     private UserAdapter userAdapter;
     UserAdapter.OnUserClickedListener onUserClickedListener;
     SwipeRefreshLayout swipeRefreshLayout;
+    String myImageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +53,11 @@ public class FriendsActivity extends AppCompatActivity {
             @Override
             public void onUserClicked(int position) {
                 Toast.makeText(FriendsActivity.this, "Tapped on user "+users.get(position).getUsername(), Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(FriendsActivity.this, MessageActivity.class));
+                startActivity(new Intent(FriendsActivity.this, MessageActivity.class)
+                        .putExtra("usernameOfRoommate", users.get(position).getUsername())
+                        .putExtra("emailOfRoommate", users.get(position).getEmail())
+                        .putExtra("imgOfRoommate", users.get(position).getProfilePicture())
+                        .putExtra("myImg", myImageUrl));
             }
         };
 
@@ -85,6 +91,13 @@ public class FriendsActivity extends AppCompatActivity {
                 recyclerView.setAdapter(userAdapter);
                 progressBar.setVisibility(View.GONE);
                 recyclerView.setVisibility(View.VISIBLE);
+
+                for (User user: users) {
+                    if (user.getEmail().equals(FirebaseAuth.getInstance().getCurrentUser().getEmail())) {
+                        myImageUrl = user.getProfilePicture();
+                        return;
+                    }
+                }
             }
 
             @Override
--- app/src/main/java/com/idv/campfire/MessageActivity.java
+++ app/src/main/java/com/idv/campfire/MessageActivity.java
@@ -3,12 +3,38 @@ package com.idv.campfire;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MessageActivity extends AppCompatActivity {
+
+    ImageView userPfp, sendButton;
+    TextView userChattingWith;
+    EditText edt_chatBox;
+    ProgressBar progressMessages;
+
+    ArrayList<Message> messages;
+    String usernameOfRoommate, emailOfRoommate, chatRoomId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        userPfp = findViewById(R.id.pfp_of_user);
+        sendButton = findViewById(R.id.send_button);
+        userChattingWith = findViewById(R.id.txt_display_name);
+        edt_chatBox = findViewById(R.id.edt_chatBox);
+        progressMessages = findViewById(R.id.progressMessages);
+
+        messages = new ArrayList<>();
+
+
+        usernameOfRoommate = getIntent().getStringExtra("usernameOfRoommate");
+        emailOfRoommate = getIntent().getStringExtra("emailOfRoommate");
     }
 }
